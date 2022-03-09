5d745386c380dfd3490f219abef98bf8ddb49c5d
Romain Dubuc <romaindubuc62@gmail.com>
2022-03-09 09:03:33 +0100

    Refresh database

--- simulateur-ejb/ejbModule/model/entity/MetricItem.java
+++ simulateur-ejb/ejbModule/model/entity/MetricItem.java
@@ -41,36 +41,12 @@ public class MetricItem {
 	 */
 	@Column(nullable = false)
 	private Double altitude;
-
-	/**
-	 * Vélocité sur l'axe x de la fusée
-	 */
-	@Column(nullable = false)
-	private Double velocity_x;
-	
-	/**
-	 * Vélocité sur l'axe y de la fusée
-	 */
-	@Column(nullable = false)
-	private Double velocity_y;
 	
 	/**
 	 * L'accélération de la fusée
 	 */
 	@Column(nullable = false)
 	private Double acceleration;
-	
-	/**
-	 * Distance vers la cible
-	 */
-	@Column(nullable = false)
-	private Double downrange_distance;
-	
-	/**
-	 * Angle de la fusée
-	 */
-	@Column(nullable = false)
-	private Double angle;
 	
 	/**
 	 * Poussée de la fusée
@@ -85,46 +61,10 @@ public class MetricItem {
 	private Double throttle;
 	
 	/**
-	 * Masse de la fusée
-	 */
-	@Column(nullable = false)
-	private Double s1_mass;
-	
-	/**
-	 * CD de la fusée
-	 */
-	@Column(nullable = false)
-	private Double cd;
-	
-	/**
-	 * Trainée de la fusée
-	 */
-	@Column(nullable = false)
-	private Double drag;
-	
-	/**
 	 * Gravité de la fusée
 	 */
 	@Column(nullable = false)
 	private Double gravity;
-	
-	/**
-	 * Différence entre la vélocité et la trainée de la fusée
-	 */
-	@Column(nullable = false)
-	private Double delta_v_drag;
-	
-	/**
-	 * Perte de gravité de la fusée
-	 */
-	@Column(nullable = false)
-	private Double gravity_loss;
-
-	/**
-	 * Delta de la vélocité
-	 */
-	@Column(nullable = false)
-	private Double delta_v;
 	
 	protected MetricItem() {
 		
@@ -170,44 +110,12 @@ public class MetricItem {
 		this.altitude = altitude;
 	}
 
-	public Double getVelocity_x() {
-		return velocity_x;
-	}
-
-	public void setVelocity_x(Double velocity_x) {
-		this.velocity_x = velocity_x;
-	}
-
-	public Double getVelocity_y() {
-		return velocity_y;
-	}
-
-	public void setVelocity_y(Double velocity_y) {
-		this.velocity_y = velocity_y;
-	}
-
 	public Double getAcceleration() {
 		return acceleration;
 	}
 
 	public void setAcceleration(Double acceleration) {
 		this.acceleration = acceleration;
-	}
-
-	public Double getDownrange_distance() {
-		return downrange_distance;
-	}
-
-	public void setDownrange_distance(Double downrange_distance) {
-		this.downrange_distance = downrange_distance;
-	}
-
-	public Double getAngle() {
-		return angle;
-	}
-
-	public void setAngle(Double angle) {
-		this.angle = angle;
 	}
 
 	public Double getThrust() {
@@ -226,59 +134,11 @@ public class MetricItem {
 		this.throttle = throttle;
 	}
 
-	public Double getS1_mass() {
-		return s1_mass;
-	}
-
-	public void setS1_mass(Double s1_mass) {
-		this.s1_mass = s1_mass;
-	}
-
-	public Double getCd() {
-		return cd;
-	}
-
-	public void setCd(Double cd) {
-		this.cd = cd;
-	}
-
-	public Double getDrag() {
-		return drag;
-	}
-
-	public void setDrag(Double drag) {
-		this.drag = drag;
-	}
-
 	public Double getGravity() {
 		return gravity;
 	}
 
 	public void setGravity(Double gravity) {
 		this.gravity = gravity;
-	}
-
-	public Double getDelta_v_drag() {
-		return delta_v_drag;
-	}
-
-	public void setDelta_v_drag(Double delta_v_drag) {
-		this.delta_v_drag = delta_v_drag;
-	}
-
-	public Double getGravity_loss() {
-		return gravity_loss;
-	}
-
-	public void setGravity_loss(Double gravity_loss) {
-		this.gravity_loss = gravity_loss;
-	}
-
-	public Double getDelta_v() {
-		return delta_v;
-	}
-
-	public void setDelta_v(Double delta_v) {
-		this.delta_v = delta_v;
 	}
 }
